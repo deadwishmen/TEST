aa4ef3d8e472d5f1c47e740f7e97185fd6c418e8
deadwishmen <86467243+deadwishmen@users.noreply.github.com>
2021-10-24 10:33:56 +0700

    Update CheckAndReport.h

--- Library/CheckAndReport.h
+++ Library/CheckAndReport.h
@@ -13,21 +13,34 @@ void CHECKANDREPORT(vector<goods> hanghoa) {
 
 	ofstream os;
 	os.open("DonHang.txt", fstream::app);
-
+	int n = 0;
 	for (i = 0; i < count;) {
-	
-		// nhap don hang
+		TableOrder(0, 0, 3 + 18);
+		vietchuoi(86, 22, "Nhan [ESC] de tro ve ", 12); 
+		vietchuoi(86, 23, "Nhan [ENTER] de tiep tuc dat hang ", 12);
 		x.number = count;
-		cin >> x.seri;
-		cin >> x.amount;					cin.ignore();
-		getline(cin, x.customerName);	cin.clear();
-		getline(cin, x.address);			cin.clear();
-		cin >> x.phoneNumber;
-		for (int i = 0; i < 3; i++) {
-			cin >> x.orderDate[i];
+		int k = keyboard();
+		if (k == 4) {
+			system("cls");
+			return;
 		}
-		x.calculateTotalMoney(hanghoa); // ham tu tinh tong tien (total money)
-
+		else {
+			vietchuoi(86, 22, "                     ", 12); //An [esc]
+			vietchuoi(50, 22, "                      ", 4); //An enter
+			textcolor(3);
+		}
+		gotoxy(2, 3+n); cout << count;
+		gotoxy(6, 3+n); cin >> x.seri;
+		gotoxy(17, 3+n); cin >> x.amount;				cin.ignore(); x.calculateTotalMoney(hanghoa);
+		gotoxy(106, 3+n); cout << x.total_money;
+		gotoxy(28, 3+n); getline(cin, x.customerName);	cin.clear();
+		gotoxy(47, 3+n); getline(cin, x.address);		cin.clear();
+		gotoxy(73, 3+n); cin >> x.phoneNumber;
+		gotoxy(88, 3+n); cin >> x.orderDate[0]; gotoxy(90, 3+n); cout << "/";
+		gotoxy(91, 3+n); cin >> x.orderDate[1]; gotoxy(93, 3+n); cout << "/";
+		gotoxy(94, 3+n); cin >> x.orderDate[2];
+		// hien thi chen
+		
 		for (int i = 0; i < hanghoa.size(); i++)
 		{
 			if (hanghoa[i].seri == x.seri)
@@ -36,31 +49,36 @@ void CHECKANDREPORT(vector<goods> hanghoa) {
 				//  hang hoa trong kho phai lon hon hoac bang so luong hang hoa nhap
 				if (hanghoa[i].amount >= x.amount)
 				{
-					// MINH ghi xuat ra tong tien : x.total_money;
 
 					hanghoa[i].amount -= x.amount;
 					os << x << endl;
 					count++;
-					cout << "Quy khach da dang ky thanh cong. Don hang dang cho xu ly";
-					//[ESC]
-					Sleep(2000);
-					system("cls");
+					gotoxy(6, 3 + n ); cout << x.seri;
+					gotoxy(17, 3 + n );cout << x.amount;
+					gotoxy(106, 3 + n );cout << x.total_money;
+					gotoxy(28, 3 + n ); cout << x.customerName;
+					gotoxy(47, 3 + n ); cout << x.address;
+					gotoxy(73, 3 + n ); cout << x.phoneNumber;
+					gotoxy(88, 3 + n ); cout << x.orderDate[0]; gotoxy(90, 3+ n ); cout << "/";
+					gotoxy(91, 3 + n ); cout << x.orderDate[1]; gotoxy(93, 3+ n ); cout << "/";
+					gotoxy(94, 3 + n ); cout << x.orderDate[2];
+					vietchuoi(50, 22, "Da Dat Hang Thanh Cong", 2);
+					n++;
 					break;
 				}
 				else {
 					test = true;
-					cout << "Hien tai khong du so luong hang hoa. Quy khach vui long chon hang hoa khac. Xin cam on";
-					Sleep(2000);
+					vietchuoi(0,22, "Hien tai khong du so luong hang hoa. Quy khach vui long chon hang hoa khac. Xin cam on", 6);
+					Sleep(3000);
 					system("cls");
 					break;
-
 				}
 			}
 		}
 		i--;
 		if (!test)
 		{
-			cout << "Ma hang khong ton tai. Vui long dien lai. Xin cam on";
+			vietchuoi(0, 22, "Ma hang khong ton tai. Vui long dien lai. Xin cam on", 6);
 			Sleep(2000);
 			system("cls");
 		}
